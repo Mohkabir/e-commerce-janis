fd0d3659f6592bea3dc1fb9c39e77676f1d446db
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-12-31 20:25:54 +0100

    new commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,13 @@
 const Footer = () => {
     return (
-        <div className='bg-[#061638] pt-[60px] pb-[30px]'>
-            <div className="max-w-6xl mx-auto">
-                <div className='w-[90%] max-w-[600px] mx-auto'>
+        <div className='bg-[#061638] pt-[60px] pb-[30px] text-sm md:text-base'>
+            <div className="max-w-6xl mx-auto w-[90%]">
+                <div className=' max-w-[600px] mx-auto'>
                     <div className='border border-white px-[20px] py-[20px] text-center'>
                         <p className='text-white capitalize'>These statement have not being evaluated by the food and drug administration. These products are intented to diagoneed to treat, cure, or prevent any disease.</p>
                     </div>
                 </div>
-                <p className='text-white text-right'>@ 2022, inno supps</p>
+                <p className='text-white text-right mt-5'>@ 2022, inno supps</p>
             </div>
         </div>
     )
