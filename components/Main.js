66b182a502d61dd04406c9a11b1159d1b2f7a404
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-12-31 17:22:31 +0100

    commit

--- components/Main.js
+++ components/Main.js
@@ -8,7 +8,7 @@ const Main = () => {
 
     return (
         <div className='flex flex-col lg:flex-row w-full my-10 mx-auto max-w-7xl'>
-            <div className="flex flex-wrap justify-between items-start gap-y-10  md:gap-x-0 md:gap-y-10 w-[90%] mx-auto">
+            <div className="flex flex-wrap justify-between items-stretch gap-y-10  md:gap-x-0 md:gap-y-10 w-[90%] mx-auto">
                 {products?.map((product) => <ProductItem key={product.id} product={product} />)}
             </div>
         </div>
--- components/ProductItem.js
+++ components/ProductItem.js
@@ -18,17 +18,17 @@ const ProductItem = ({ product }) => {
   const checkCart = cart?.find((cart) => product.id === cart.id)
 
   return (
-    <main className="flex flex-col w-[48%] h-[100%] shadow-md md:w-[30%] overflow-hidden bg-white">
-      <div className="flex justify-center py-10 border border-t-0 border-x-0 border-b-[#e8e8e8] w-[90%] mx-auto">
+    <main className="flex flex-col w-[48%] shadow-md md:w-[32%] overflow-hidden bg-white">
+      <div className="flex justify-center h-[200px] sm:h-[260px] md:h-[270px] lg:h-[340px] py-10 border border-t-0 border-x-0 border-b-[#e8e8e8] w-[90%] mx-auto">
         <Image src={product.img} className="w-[100%] h-[100%]" alt="product" width={300} height={300} />
       </div>
 
-      <div className="py-5 px-3  rounded-md flex flex-col gap-1">
+      <div className="py-5 px-3 rounded-md flex flex-col gap-1">
         <p className="text-lg text-center font-medium">{product.title}</p>
 
         <div className="flex flex-col text-center items-center gap-3">
           <p className="font-bold text-2xl">#{convertString(product.price)} NGN</p>
-          <div className="flex flex-wrap gap-3 items-center">
+          <div className="flex flex-wrap justify-center gap-3 items-center">
             <p className="font-bold text-base"><del>#{convertString(product.formerPrice)}</del></p>
             <p className="text-green-600 text-sm font-semibold">%{product.discount} off</p>
           </div>
@@ -47,8 +47,8 @@ const ProductItem = ({ product }) => {
         {/* <p className="text-white">{product.star}</p><AiFillStar /> */}
 
         <div className="mt-7">
-          <Link href={`product/${product.id}`}>
-            <button className="bg-blue text-white w-full p-2 rounded-md font-bold text-md md:text-lg md:flex-1"><AiOutlineShoppingCart className="inline mb-1 mr-1" /> {checkCart?.inCart ? "in cart" : "Add To Cart"}</button>
+          <Link href={`product/${product.id}`} className="text-sm sm:text-lg">
+            <button className="bg-blue text-white w-full p-2 rounded-md font-bold  md:flex-1"><AiOutlineShoppingCart className="inline mb-1 mr-1" /> {checkCart?.inCart ? "in cart" : "Add To Cart"}</button>
           </Link>
         </div>
       </div>
